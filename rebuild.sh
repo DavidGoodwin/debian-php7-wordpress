51e52d921c479680d09cd8ee8e159a8bc67f18c7
David Goodwin <david@palepurple.co.uk>
2017-11-14 12:12:12 +0000

    fix pwd etc

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,8 @@
 
 set -exu
 
+cd $(dirname $0)
+
 docker build --rm --pull -t davidgoodwin/jessie-php7-wordpress:build-$(date +%F) -t davidgoodwin/jessie-php7-wordpress:latest .
 
 docker push davidgoodwin/jessie-php7-wordpress:build-$(date +%F)
