09936d235988def73eaf6827cb15428c966d2c0c
David Goodwin <david@palepurple.co.uk>
2019-02-01 19:36:14 +0000

    do not push

--- rebuild.sh
+++ rebuild.sh
@@ -6,9 +6,5 @@ cd $(dirname $0)
 
 docker build --rm --no-cache --pull -t davidgoodwin/jessie-php7-wordpress:build-$(date +%F) -t davidgoodwin/jessie-php7-wordpress:latest .
 
-docker push davidgoodwin/jessie-php7-wordpress:build-$(date +%F)
-docker push davidgoodwin/jessie-php7-wordpress:latest
-
-
-
-
+# docker push davidgoodwin/jessie-php7-wordpress:build-$(date +%F)
+# docker push davidgoodwin/jessie-php7-wordpress:latest
