5336ce32589b31d5719aacda8cb6547e57e69ccb
David Goodwin <david@codepoets.co.uk>
2021-03-22 09:07:54 +0000

    whatever

--- rebuild.sh
+++ rebuild.sh
@@ -6,7 +6,9 @@ cd $(dirname $0)
 
 docker login -u davidgoodwin -p gingersharkbeans99
 
-docker build --rm --no-cache --pull -t davidgoodwin/stretch-php7-wordpress:build-$(date +%F) -t davidgoodwin/stretch-php7-wordpress:latest .
+docker build --rm --no-cache --pull \
+     -t davidgoodwin/debian-php7-wordpress:build-$(date +%F) \
+     -t davidgoodwin/debian-php7-wordpress:latest .
 
-docker push davidgoodwin/stretch-php7-wordpress:build-$(date +%F)
-docker push davidgoodwin/stretch-php7-wordpress:latest
+docker push davidgoodwin/debian-php7-wordpress:build-$(date +%F)
+docker push davidgoodwin/debian-php7-wordpress:latest
